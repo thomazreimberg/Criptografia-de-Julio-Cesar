8f81b42ee31e72629447c488b063b0985d7d1a43
thomazreimberg <treimberg7@gmail.com>
2020-06-03 00:18:10 -0300

    Application settings

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de usuários');
+
+    response.send('Hello World');
+});
+
+app.listen(3333);
