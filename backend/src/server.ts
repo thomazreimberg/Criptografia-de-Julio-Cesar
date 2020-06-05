1f1326fc9aff374d387f78ddd68ef5aba493bbed
Thomaz Henrique <treimberg7@gmail.com>
2020-06-05 08:44:42 -0300

    axios error (response)

---
+++ backend/src/server.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+const app = express();
+const fs = require('fs');
+const axios = require('axios').default;
+import {AxiosResponse} from 'axios';
+
+const token = require('./others/token.ts').token;
+const sha1 = require('./utils/sha1.ts');
+const decode3 = require('./utils/decode3.ts');
+
+interface Response {
+    numero_casas : number
+    token: string
+    cifrado: string
+    decifrado : string
+    resumo_criptografico: string
+}
+
+app.get('/crypto', () => {
+    axios.get(
+        `https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${token}`)
+        .then(response => {
+        let { numero_casas, cifrado, resumo_criptografico } = response.data;
+        console.log(numero_casas);
+        let message = decode3(cifrado, numero_casas);
+        console.log('output: ' + message);
+        
+        let resumo = sha1(message);
+
+        const answer = {
+            numero_casas,
+            cifrado,
+            decifrado: message,
+            resumo_criptografico: resumo,
+        };
+
+        console.log(answer);
+
+        fs.writeFile(__dirname + "/anser.json"), JSON.stringify(answer), (error: String) => {
+            console.log(error || 'Saved file.');
+        };
+        console.log(response);
+    });
+});
+
+app.listen(3333);
